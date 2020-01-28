fad0d8d1542b707cdeb192501ad0a43ba4036b5f
William Barela <william.barela.dev@gmail.com>
2020-01-28 04:24:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let submitter = document.querySelectorAll("input")[1];
-  let todos = document.createElement('ul');
+  let tasks = document.getElementById('tasks');
   
   submitter.addEventListener('click', (e) => {
       e.preventDefault();
