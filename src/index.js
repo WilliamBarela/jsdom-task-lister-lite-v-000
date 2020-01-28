a20a421e8c0b92d3af8251facc16976ebca5d334
William Barela <william.barela.dev@gmail.com>
2020-01-28 04:18:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let submitter = document.querySelectorAll("input")[1];
   
-  
+  submitter.addEventListener('click', (e) => {
+      
+    })
 });
